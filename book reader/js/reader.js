94f9c42020df3899a21b3da8b3c49bf77714fa72
Sugarcone97 <37585767+Sugarcone97@users.noreply.github.com>
2021-01-25 21:45:37 +0100

    Add files via upload

---
+++ book reader/js/reader.js
@@ -0,0 +1,43 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+
+ if (n > x.length) {slideIndex = 1}   
+  if (n < 1) {slideIndex = x.length}
+
+  if (i < 0) { i = 0; }                 
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  
+for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-red";
+}
